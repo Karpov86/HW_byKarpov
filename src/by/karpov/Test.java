8a750efa7cd8bf1b8c8175a0dc593ea776362091
User <karpoff19@mail.ru>
2020-01-30 20:19:07 +0300

    30.01.2020

--- src/by/karpov/Test.java
+++ src/by/karpov/Test.java
@@ -7,12 +7,6 @@ public class Test {
 
     public static void main(String[] args) {
 
-        Date date = new Date();
-        System.out.println(date);
 
-        LocalDate localDate = LocalDate.now();
-        LocalDate localDate1;
-        System.out.println(localDate.getYear());
-        System.out.println(localDate);
     }
 }
